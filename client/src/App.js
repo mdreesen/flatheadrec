242b602f5525bca284820936a770cb35f871ed02
Michael Dreesen <michaeldreesen90@gmail.com>
2021-09-22 21:34:13 -0400

    adding frontend routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-   <div>
-     <h1>app has started</h1>
-   </div>
+    <div>
+      <h1>app has started</h1>
+      <Router>
+        <div className="container">
+          <Switch>
+
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 }
 
