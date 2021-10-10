4a394c73fa4596ca988f8b320b96d260ee31a845
Michael Dreesen <michaeldreesen90@gmail.com>
2021-10-10 11:01:09 -0400

    adding coffee shop mapping

--- client/src/pages/Places/Coffee/index.js
+++ client/src/pages/Places/Coffee/index.js
@@ -3,13 +3,26 @@ import { useQuery } from '@apollo/react-hooks';
 import { QUERY_PLACES } from '../../../utils/queries';
 
 import NavbarUser from '../../../components/NavbarUser';
+import Places from '../../../components/Places';
 
 const Coffee = () => {
     const { data } = useQuery(QUERY_PLACES);
+    console.log(data.places)
+
+    const CoffeeShop = () => {
+        return data.places.map((coffee, index) => {
+            console.log(coffee.type === 'Coffee Shop')
+            return coffee.type === 'Coffee Shop'; 
+        })
+    }
+
+    console.log(Places);
+
     return (
         <div>
         <NavbarUser />
         Here is the coffee page
+        <Places/>
         </div>
     );
 }
